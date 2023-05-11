f57d9eadfa2e9c5f4a607e57eef00b33bf73933e
Hetal Rathod <hetalrathod405@gmail.com>
2023-05-11 17:01:19 +0530

    Added Validation

--- src/main/java/com/mPokket/phonebook/exception/PhoneBookServiceExceptions.java
+++ src/main/java/com/mPokket/phonebook/exception/PhoneBookServiceExceptions.java
@@ -4,6 +4,7 @@ import java.util.NoSuchElementException;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.client.HttpClientErrorException.NotFound;
@@ -23,6 +24,14 @@ public class PhoneBookServiceExceptions {
 		log.error(Constants.EXCEPTION_LOG, ex);
 		Response response = Response.builder().errorCode(HttpStatus.NOT_FOUND.value()).errorMessage(HttpStatus.NOT_FOUND.getReasonPhrase()).build();
 		return new ResponseEntity<Response>(response, HttpStatus.NOT_FOUND);		
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<Response> handleNotValidException(MethodArgumentNotValidException ex){
+		
+		log.error(Constants.EXCEPTION_LOG, ex);
+		Response response = Response.builder().errorCode(HttpStatus.BAD_REQUEST.value()).errorMessage(ex.getLocalizedMessage()).build();
+		return new ResponseEntity<Response>(response, HttpStatus.BAD_REQUEST);		
 	}
 	
 	@ExceptionHandler(Exception.class)
